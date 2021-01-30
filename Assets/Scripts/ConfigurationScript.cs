cb16b78a8baff530ed76ec0f557d85318e905895
Alan Floriano <alanfloriano@hotmail.com>
2021-01-29 21:02:50 -0300

    Criando o Main Menu, menu de configuraçoes e vinculando tudo a um mixer.
    Criando o Main Menu, menu de configuraçoes e vinculando tudo a um mixer.

---
+++ Assets/Scripts/ConfigurationScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class ConfigurationScript : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+    public Slider volumeSlider;
+    public Slider effectsSlider;
+
+    void Start ()
+    {
+        setInitialValues();
+    }
+
+    private void setInitialValues()
+    {
+        mainMixer.SetFloat("MainVolume", 0.0f);
+        mainMixer.SetFloat("EffectsVolume", 0.0f);
+        effectsSlider.value = 0;
+        volumeSlider.value = 0;
+    }
+
+    public void SetVolume (float volume)
+    {
+        mainMixer.SetFloat("MainVolume", volume);
+    }
+    public void SetEffects (float effects)
+    {
+        mainMixer.SetFloat("EffectsVolume", effects);
+    }
+}
